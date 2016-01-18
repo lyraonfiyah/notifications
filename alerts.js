e3c576fb43d0e6e4f16aeb5921668c776e0a25ba
Meg Stepp <mcstepp@gmail.com>
2016-01-18 00:05:03 -0500

    remove debugging statements

--- alerts.js
+++ alerts.js
@@ -8,80 +8,71 @@ window.addEventListener('load', function() {
     }
 });
 
-$(document).ready(function() {
+function notifyAlerts(data, status, title) {
+    var savedStr = localStorage.getItem("n") || "";
+    var dirtyStr = ""; 
+    var icon, text, aid;
+    var alertsLength = data ? Object.keys(data).length : 0;
 
-    function notifyAlerts(data, status, title) {
-        var savedStr = localStorage.getItem("n") || "";
-        var dirtyStr = ""; 
-        var icon, text, aid;
-        var alertsLength = data ? Object.keys(data).length : 0;
+    if (Notification.permission === "granted" && alertsLength > 0) { 
+        for (var j = 0; j < alertsLength; j++){ 
+            var daid = data[j].alert_id; 
+            dirtyStr += daid; 
+        } 
+        for (var i = 0; i < alertsLength; i++) { 
+            var linkText = data[i].text; 
+            var iconText = data[i].icon; 
+            var url = data[i].primary_url; 
+            var iconBegin = iconText.indexOf("http"); 
+            var iconEnd = iconText.indexOf("' border"); 
+            icon = iconText.substring(iconBegin, iconEnd) || null; 
+            text = jQuery(linkText).text(); aid = data[i].alert_id;
 
-        if (Notification.permission === "granted" && alertsLength > 0) { 
-            for (var j = 0; j < alertsLength; j++){ 
-                var daid = data[j].alert_id; 
-                dirtyStr += daid; 
+            if (savedStr.indexOf(aid) < 0) { 
+                spawnNotification(title, text, icon, aid, url); 
             } 
-            for (var i = 0; i < alertsLength; i++) { 
-                var linkText = data[i].text; 
-                var iconText = data[i].icon; 
-                var url = data[i].primary_url; 
-                var iconBegin = iconText.indexOf("http"); 
-                var iconEnd = iconText.indexOf("' border"); 
-                icon = iconText.substring(iconBegin, iconEnd) || null; 
-                text = jQuery(linkText).text(); aid = data[i].alert_id;
-
-                if (savedStr.indexOf(aid) < 0) { 
-                    spawnNotification(title, text, icon, aid, url); 
-                } 
-            }
-
-            localStorage.setItem("n",dirtyStr);  
         }
+
+        localStorage.setItem("n",dirtyStr);  
+    }
+}
+
+function spawnNotification(title, text, icon, tag, url) {
+    var options = {
+        body: text,
+        icon: icon,
+        tag: tag
+    };
+    var n = new Notification(title, options);
+    setTimeout(n.close.bind(n), 10000);
+    n.onclick = function() {
+        window.open(url, '_blank');
+    };
+}
+
+function customAlerts(a, b){
+    if (!a) {
+        throw "Error: "+a+" must be a string.";
     }
 
-    function spawnNotification(title, text, icon, tag, url) {
-        var options = {
-            body: text,
-            icon: icon,
-            tag: tag
-        };
-        var n = new Notification(title, options);
-        setTimeout(n.close.bind(n), 10000);
-        n.onclick = function() {
-            window.open(url, '_blank');
-        };
+    if (typeof a !== 'string') {
+        throw "Error: "+a+" must be a string.";
     }
 
-    function customAlerts(a, b){
-        if (!a) {
-            throw "Error: "+a+" must be a string.";
-        }
-
-        if (typeof a !== 'string') {
-            throw "Error: "+a+" must be a string.";
-        }
-
-        else {
+    else {
         var title = a;
         var interval = (b && typeof b === 'number' && b > 15000) ? b : 15000;
-
-        window.setInterval(function() {
-        console.log('polling every '+interval+' milliseconds...');
-        jQuery.ajax({
+        var options = {
             url: 'index.php?recent_alerts=1&js=1',
             dataType: 'json',
             success: function (data, status) {
                 notifyAlerts(data, status, title);
             }
-        });
-    }, interval);
+        };
+
+        window.setInterval(function() {
+            console.log('polling every '+interval+' milliseconds...');
+            jQuery.ajax(options);
+        }, interval);
     }
-    }
-
-    /********
-    Invoke the call
-    *********/
-
-    customAlerts(["title", Num interval]);
-
-});
+}
