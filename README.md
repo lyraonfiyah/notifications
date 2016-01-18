50698dc6690f8171b099521b4b2fffbdada50d8b
Meg Stepp <mcstepp@gmail.com>
2016-01-17 23:57:07 -0500

    fixed and added notifyAlerts

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# jcink-scripts
-Various scripts and templates created specifically for Jcink / JFS.
+# Jcink Scripts
+Various scripts and templates created specifically for Jcink / JFS. The Jcink mods are typically Javascript plugins used to enhance the features provided by Jcink. I also try to make these compatible with other third-party modifications, especially [CFS](https://github.com/ConnorWiseman/jcink-custom-structure). 
+
+I'm also (very) open to code reviews and improvements. Javascript is my preferred language. 
+
+## Alerts
--- alerts.js
+++ alerts.js
@@ -10,43 +10,78 @@ window.addEventListener('load', function() {
 
 $(document).ready(function() {
 
-    function spawnNotification(text, icon, tag, url) {
+    function notifyAlerts(data, status, title) {
+        var savedStr = localStorage.getItem("n") || "";
+        var dirtyStr = ""; 
+        var icon, text, aid;
+        var alertsLength = data ? Object.keys(data).length : 0;
+
+        if (Notification.permission === "granted" && alertsLength > 0) { 
+            for (var j = 0; j < alertsLength; j++){ 
+                var daid = data[j].alert_id; 
+                dirtyStr += daid; 
+            } 
+            for (var i = 0; i < alertsLength; i++) { 
+                var linkText = data[i].text; 
+                var iconText = data[i].icon; 
+                var url = data[i].primary_url; 
+                var iconBegin = iconText.indexOf("http"); 
+                var iconEnd = iconText.indexOf("' border"); 
+                icon = iconText.substring(iconBegin, iconEnd) || null; 
+                text = jQuery(linkText).text(); aid = data[i].alert_id;
+
+                if (savedStr.indexOf(aid) < 0) { 
+                    spawnNotification(title, text, icon, aid, url); 
+                } 
+            }
+
+            localStorage.setItem("n",dirtyStr);  
+        }
+    }
+
+    function spawnNotification(title, text, icon, tag, url) {
         var options = {
             body: text,
             icon: icon,
             tag: tag
         };
-        var n = new Notification("<% TITLE %>", options);
-
-        setTimeout(n.close.bind(n), 10000);
-
-        n.onclick = function() {
-            window.open(url, '_blank');
-        };
-    }
-
-    function spawnNotification(text, icon, tag, url) {
-        var options = {
-            body: text,
-            icon: icon,
-            tag: tag
-        };
-        var n = new Notification("<% TITLE %>", options);
+        var n = new Notification(title, options);
         setTimeout(n.close.bind(n), 10000);
         n.onclick = function() {
             window.open(url, '_blank');
         };
     }
 
-    window.setInterval(function() {
-        console.log('poll...');
+    function customAlerts(a, b){
+        if (!a) {
+            throw "Error: "+a+" must be a string.";
+        }
+
+        if (typeof a !== 'string') {
+            throw "Error: "+a+" must be a string.";
+        }
+
+        else {
+        var title = a;
+        var interval = (b && typeof b === 'number' && b > 15000) ? b : 15000;
+
+        window.setInterval(function() {
+        console.log('polling every '+interval+' milliseconds...');
         jQuery.ajax({
             url: 'index.php?recent_alerts=1&js=1',
             dataType: 'json',
             success: function (data, status) {
-                notifyAlerts(data, status);
+                notifyAlerts(data, status, title);
             }
         });
-    }, 15000);
+    }, interval);
+    }
+    }
+
+    /********
+    Invoke the call
+    *********/
+
+    customAlerts(["title", Num interval]);
 
 });
